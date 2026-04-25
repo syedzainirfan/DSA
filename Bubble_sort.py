616a7b8b07fb33c4fbe2124b686bbf31be15a8bb
SYED ZAIN IRFAN <109850884+syedzainirfan@users.noreply.github.com>
2026-04-25 18:23:03 +0500

    Add files via upload

---
+++ Bubble_sort.py
@@ -0,0 +1,14 @@
+# Go through the array, one value at a time.
+# For each value, compare the value with the next value.
+# If the value is higher than the next one, swap the values so that the highest value comes last.
+# Go through the array as many times as there are values in the array
+
+mylist = [64, 34, 25, 12, 22, 11, 90, 5]
+
+n = len(mylist)
+for i in range(n-1):
+  for j in range(n-i-1):
+    if mylist[j] > mylist[j+1]:
+      mylist[j], mylist[j+1] = mylist[j+1], mylist[j]
+
+print(mylist)
